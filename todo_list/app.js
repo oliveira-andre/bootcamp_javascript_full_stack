a33057a05ee49da41f389ab1f98711c2884df1e0
oliveira-andre <desmonplays@gmail.com>
2019-12-25 13:47:03 -0400

    (new feature) title of todo list

--- todo_list/app.js
+++ todo_list/app.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>TODO List</h1>')
+});
+
+app.listen(3000, () => {
+  console.log('Server is up');
+});
